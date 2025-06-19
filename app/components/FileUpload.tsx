0bfb54e3cdc2813631ab7e6ace288498c788c472
Sarowar-h <sarw838dev@gmail.com>
2025-06-19 11:09:46 +0600

    api client done

--- app/components/FileUpload.tsx
+++ app/components/FileUpload.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { upload } from "@imagekit/next";
 import { useState } from "react";
 
 // This component must be a client component
@@ -27,7 +28,41 @@ const FileUpload = ({ onSuccess, onProgress, fileType }) => {
     };
 
     const handleFileChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
-        e.preventDefault();
+        const file = e.target.files?.[0];
+        if (!file || !validateFile(file)) {
+            return;
+        }
+        setUploading(true);
+        setError(null);
+
+        try {
+            const authRes = await fetch("/api/auth/imagekit-auth");
+            const auth = await authRes.json();
+
+            const response = await upload({
+                file,
+                fileName: file.name,
+                publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY!,
+                signature: auth.signature,
+                expire: auth.expire,
+                token: auth.token,
+
+                onProgress: (event) => {
+                    if (event.lengthComputable && setProgress) {
+                        const persent = (event.loaded / event.total) * 100;
+
+                        onProgress(Math.round(persent));
+                    }
+                },
+                abortSignal: abortController.signal,
+            });
+            onSuccess(response);
+        } catch (error) {
+            console.error("Upload failed:", error);
+            setError("Upload failed. Please try again.");
+        } finally {
+            setUploading(false);
+        }
     };
 
     return (
